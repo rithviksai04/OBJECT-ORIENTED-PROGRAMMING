4860a202b630bc0e4b647f90e403a7fcbe3aae57
rithviksai04 <nagamallyrithviksai@gmail.com>
2025-07-25 11:11:04 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,11 @@
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int X=10;
+        int Y=20;
+        int Z = X + Y;
+        System.out.println(Z);
+	}
+
+}
